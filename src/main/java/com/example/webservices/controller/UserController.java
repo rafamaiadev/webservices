c007b59a0b78e36eb97b58be0de2e30e1279ef83
rafamaiadev <119112502+rafamaiadev@users.noreply.github.com>
2023-08-08 09:39:31 -0300

    UserModel and UserController

---
+++ src/main/java/com/example/webservices/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.webservices.controller;
+
+import com.example.webservices.domain.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+    public ResponseEntity<User> findAll() {
+    }
+}
