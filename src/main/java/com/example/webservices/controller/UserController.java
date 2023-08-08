86d3ad8e8c6b39a1867798960238f95b1b807301
rafamaiadev <119112502+rafamaiadev@users.noreply.github.com>
2023-08-08 11:00:36 -0300

    att user controller, service and repository

--- src/main/java/com/example/webservices/controller/UserController.java
+++ src/main/java/com/example/webservices/controller/UserController.java
@@ -1,12 +1,29 @@
 package com.example.webservices.controller;
 
 import com.example.webservices.domain.model.User;
+import com.example.webservices.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
+    private final UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        return ResponseEntity.ok().body(userService.findAll());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable(name = "id") Long id) {
+        return ResponseEntity.ok().body(userService.findById(id));
+    }
 }
--- src/main/java/com/example/webservices/service/UserService.java
+++ src/main/java/com/example/webservices/service/UserService.java
@@ -1,4 +1,26 @@
 package com.example.webservices.service;
 
+import com.example.webservices.domain.model.User;
+import com.example.webservices.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> user = Optional.ofNullable(userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("id not found")));
+        return user.get();
+    }
 }
